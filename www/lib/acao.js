39c3e6da3423534cdcf64a5e2150a12c7762ec10
luazrk <luazrk@gmail.com>
2020-06-21 11:31:06 +0900

    Adicionando novas telas.

--- www/lib/acao.js
+++ www/lib/acao.js
@@ -2,3 +2,28 @@
 $(document).on("click","#Pedir", function(){
 $(location).attr("href", "pedirajuda.html");
 });
+
+//ações de mudança de tela pedir ajuda
+$(document).on("click","#Preciso", function(){
+$(location).attr("href", "precisodeajuda.html");
+});
+
+//ações de mudança de tela dependentes
+$(document).on("click","#PedirAjuda", function(){
+$(location).attr("href", "dependentes.html");
+});
+
+//ações de mudança de tela adicionando dependente
+$(document).on("click","#addepe", function(){
+$(location).attr("href", "adicionadependentes.html");
+});
+
+//ações de mudança de tela adicionando mais dependentes
+$(document).on("click","#Cadastrardepe", function(){
+$(location).attr("href", "dependentes.html");
+});
+
+//ações de mudança de tela continuar pedido
+$(document).on("click","#continuar", function(){
+$(location).attr("href", "pedido.html");
+});
