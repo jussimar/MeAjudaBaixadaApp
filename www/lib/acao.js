41b0e32f02ad06e61b750033451eccb84930f779
anabeatrizzz <anaaugusto2012@bol.com.br>
2020-06-22 18:59:35 -0300

    deixando parecido com o layout web

--- www/lib/acao.js
+++ www/lib/acao.js
@@ -1,39 +1,6 @@
-//ações de mudança de tela
-$(document).on("click","#Pedir", function(){
-$(location).attr("href", "pedirajuda.html");
-});
-
-//ações de mudança de tela pedir ajuda
-$(document).on("click","#Preciso", function(){
-$(location).attr("href", "precisodeajuda.html");
-});
-
-//ações de mudança de tela dependentes
-$(document).on("click","#PedirAjuda", function(){
-$(location).attr("href", "dependentes.html");
-});
-
-//ações de mudança de tela adicionando dependente
-$(document).on("click","#addepe", function(){
-$(location).attr("href", "adicionadependentes.html");
-});
-
-//ações de mudança de tela adicionando mais dependentes
-$(document).on("click","#Cadastrardepe", function(){
-$(location).attr("href", "dependentes.html");
-});
-
-//ações de mudança de tela continuar pedido
-$(document).on("click","#continuar", function(){
-$(location).attr("href", "pedido.html");
-});
-
-//ações de mudança de tela ajudar
-$(document).on("click","#Ajudar", function(){
-$(location).attr("href", "ajudar.html");
-});
-
-//ações de mudança de tela lista de pessoas
-$(document).on("click","#Listadepessoas", function(){
-$(location).attr("href", "listapessoas.html");
-});
+$(function(){
+	$("#cadastrar").click(function(){
+		alert("Cadastro feito com sucesso! Faça seu login e cadastre seus dependentes (caso tiver)")
+		window.location.href = "../www/index.html"
+	})
+})
