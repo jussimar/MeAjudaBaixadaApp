3c8ab02f6585f2543be6a6ee633b917e52cd448b
anabeatrizzz <anaaugusto2012@bol.com.br>
2020-06-23 12:33:22 -0300

    logica listapessoas, pagina login e usuario

--- www/lib/acao.js
+++ www/lib/acao.js
@@ -1,4 +1,13 @@
 $(function(){
+	$("#cardComInfos").hide();
+	$("#listagem").change(function(){
+		if($("#listagem").val() != "EUC"){
+			$("#cardComInfos").show();
+		} else {
+			$("#cardComInfos").hide();
+		}
+	})
+
 	$("#cadastrar").click(function(){
 		alert("Cadastro feito com sucesso! Faça seu login e cadastre seus dependentes (caso tiver)")
 		window.location.href = "../www/index.html"
