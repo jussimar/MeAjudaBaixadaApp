76ab3b64b94f3e323f6f14b816a8cb558300d702
anabeatrizzz <anaaugusto2012@bol.com.br>
2020-06-23 21:01:26 -0300

    ajustando alguns detalhes

--- www/lib/acao.js
+++ www/lib/acao.js
@@ -13,17 +13,18 @@ $(function(){
 	$("#cadastrar").click(function(){
 		alert("Cadastro feito com sucesso! Faça seu login e cadastre seus dependentes (caso tiver)")
 		window.location.href = "../www/index.html"
+	})
 
 	//ações adicionar dependentes
 	let d = $("[id^='dd']").length;
-		$("#mais").click(function(){
-			d++;
-			$("#dependentespedido").append(`<div id="dd${d}"><input class="form-control border-purple" placeholder="Nome do dependente" type="text" name="nm_dependente"><br><input class="form-control border-purple" type="date" name="dt_nascimento"><br><input placeholder="CPF do dependente" class="form-control border-purple" type="number" name="cd_cpf"><br></div>`)
-		})
-		$("#menos").click(function(){
-			$("[id^='dd']:last-child").remove()
-	  })
-		})
+	$("#mais").click(function(){
+		d++;
+		$("#dependentespedido").append(`<div id="dd${d}"><input class="form-control border-purple" placeholder="Nome do dependente" type="text" name="nm_dependente"><br><input class="form-control border-purple" type="date" name="dt_nascimento"><br><input placeholder="CPF do dependente" class="form-control border-purple" type="number" name="cd_cpf"><br></div>`)
+	})
+	$("#menos").click(function(){
+		$("[id^='dd']:last-child").remove()
+  })
+		
 
 	//ações de mudança de tela cadastro amigo
 	$(document).on("click","#parceiro.html", function(){
